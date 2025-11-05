f5024666e2dc9b7408b33553ffe768c88069106b
unknown <samuel.imex@gmail.com>
2025-11-05 07:55:58 +0100

    fixes based on ongoing test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "All good! Let's get started";
+    return "Testing Deployment to staging"  //"All good! Let's get started";
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -150,7 +150,7 @@ async function bootstrap() {
   const metricsPort = process.env.METRICS_PORT || 9100;
   metricsApp.listen(metricsPort, () =>
     appLogger.log(
-      `✅ Prometheus metrics available on: http://localhost:${metricsPort}/metrics`,
+      `✅ Prometheus metrics available on: http://${process.env.BACKEND_URI}:${metricsPort}/metrics`,
     ),
   );
 
@@ -162,12 +162,12 @@ async function bootstrap() {
   const port = process.env.PORT || 4000;
   await app.listen(port);
 
-  appLogger.log(`🚀 Application running on: http://localhost:${port}/api`);
+  appLogger.log(`🚀 Application running on: http://${process.env.BACKEND_URI}:${port}/api`);
   appLogger.log(
-    `📘 Swagger docs available at: http://localhost:${port}/api/docs`,
+    `📘 Swagger docs available at: http://${process.env.BACKEND_URI}:${port}/api/docs`,
   );
   appLogger.log(
-    `📊 Metrics available at: http://localhost:${metricsPort}/metrics`,
+    `📊 Metrics available at: http://${process.env.BACKEND_URI}:${metricsPort}/metrics`,
   );
 
   // ================= Graceful Exit (Optional Extra Safety) =================
