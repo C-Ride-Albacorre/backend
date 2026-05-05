2825b6a7387a6e947d194f78324e21913e9e999b
Ime Samuel <samuel.imex@gmail.com>
2026-05-05 14:18:45 +0100

    updating HEAD route

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,14 @@ async function bootstrap() {
     bufferLogs: true,
   });
 
+  // ✅ HEAD
+  app.use((req, res, next) => {
+    if (req.method === 'HEAD' && req.url === '/') {
+      return res.status(200).end();
+    }
+    next();
+  });
+
   const config = app.get(ConfigService);
 
   if (config.get('ENABLE_VIEWS') === 'true') {
