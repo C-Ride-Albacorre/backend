0cea4bddd2305e86c6fc3abeee128d4ce425a323
Ime Samuel <samuel.imex@gmail.com>
2026-06-01 18:00:56 +0100

    updated password match regex

--- src/modules/auth/dto/create-vendor.dto.ts
+++ src/modules/auth/dto/create-vendor.dto.ts
@@ -34,7 +34,11 @@ export class CreateVendorDto {
   @ApiProperty({ example: 'StrongP@ssw0rd' })
   @IsString()
   @MinLength(8)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/)
+  //@Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+    message:
+      'Password must contain at least 1 uppercase, 1 lowercase, 1 number and 1 special character',
+  })
   password: string;
 
   @ApiProperty({ example: 'John' })
--- src/modules/auth/dto/reset-password-with-otp.dto.ts
+++ src/modules/auth/dto/reset-password-with-otp.dto.ts
@@ -19,7 +19,8 @@ export class ResetPasswordWithOtpDto {
   })
   @IsString()
   @MinLength(8)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/, {
+  //@Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/, {
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
     message:
       'Password must contain at least 1 uppercase, 1 lowercase, 1 number and 1 special character',
   })
