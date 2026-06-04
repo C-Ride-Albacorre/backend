ca8067e69394fa3b31a032c0268a53f6bd06adc0
Ime Samuel <samuel.imex@gmail.com>
2026-06-04 10:26:56 +0100

    fixed vendor accept bug

--- src/modules/order/dto/vendor-action.dto.ts
+++ src/modules/order/dto/vendor-action.dto.ts
@@ -1,21 +1,32 @@
-import { Optional } from '@nestjs/common';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+
+
+export enum VendorAction {
+  ACCEPT = 'ACCEPT',
+  DECLINE = 'DECLINE',
+}
+
 
 export class VendorActionDto {
-@ApiProperty({
-    enum: ['ACCEPT', 'DECLINE'],
-    description: 'Vendor action type',
-    example: 'ACCEPT | DECLINE',
+@ApiPropertyOptional({
+    description: 'Action to perform on the order',
+    enum: VendorAction,
+    example: VendorAction.ACCEPT,
   })
-  @Optional()
-  @IsEnum(['ACCEPT', 'DECLINE'])
-  action: 'ACCEPT' | 'DECLINE';
+   @ApiProperty({
+    enum: VendorAction,
+    example: VendorAction.ACCEPT,
+  })
+  @IsEnum(VendorAction)
+  action: VendorAction;
 
+  
   @ApiPropertyOptional({
     description: 'Reason for declining or providing context for the action',
     example: 'Out of stock',
   })
   @IsString()
+  @IsOptional()
   reason?: string;
 }
