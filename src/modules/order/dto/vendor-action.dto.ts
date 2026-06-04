7ebc26021dd1c0ec8e6d01ba22a883ec81a41b10
Ime Samuel <samuel.imex@gmail.com>
2026-06-04 10:46:39 +0100

    fixed vendor accept bug

--- src/modules/order/dto/vendor-action.dto.ts
+++ src/modules/order/dto/vendor-action.dto.ts
@@ -21,12 +21,11 @@ export class VendorActionDto {
   @IsEnum(VendorAction)
   action: VendorAction;
 
-  
+
   @ApiPropertyOptional({
     description: 'Reason for declining or providing context for the action',
     example: 'Out of stock',
   })
-  @IsString()
   @IsOptional()
   reason?: string;
 }
