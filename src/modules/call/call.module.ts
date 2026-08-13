e44b84d6d792469655760220dce65a1df51f74a1
Ime Samuel <samuel.imex@gmail.com>
2026-08-13 17:58:27 +0100

    updated  call module

--- src/modules/call/call.module.ts
+++ src/modules/call/call.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CallService } from './call.service';
 import { CallController } from './call.controller';
+import { NotificationModule } from '../notification/notification.module';
+import { PushNotificationService } from '../notification/push-notification.service';
+import { AgoraTokenService } from './provider/agora-token.service';
 
 @Module({
+  imports: [NotificationModule],
   controllers: [CallController],
-  providers: [CallService],
+  providers: [
+    CallService,
+    PushNotificationService,
+    AgoraTokenService,
+  ],
 })
 export class CallModule {}
