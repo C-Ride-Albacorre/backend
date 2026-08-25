c33efe28d8c2c9f3017bd86bf046ce03e499c02f
Ime Samuel <samuel.imex@gmail.com>
2026-08-25 17:24:52 +0100

    updated the eta update service

--- src/modules/admin/dto/update-customer-status.dto.ts
+++ src/modules/admin/dto/update-customer-status.dto.ts
@@ -1,9 +1,22 @@
+// import { IsEnum } from 'class-validator';
+// import { UserStatus } from '@prisma/client';
+// import { ApiProperty } from '@nestjs/swagger';
+
+// export class UpdateCustomerStatusDto {
+//   @ApiProperty({ enum: UserStatus, required: true })
+//   @IsEnum(UserStatus)
+//   status: UserStatus;
+// }
+
 import { IsEnum } from 'class-validator';
 import { UserStatus } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateCustomerStatusDto {
-  @ApiProperty({ enum: UserStatus, required: true })
-  @IsEnum(UserStatus)
+  @ApiProperty({
+    enum: [UserStatus.ACTIVE, UserStatus.SUSPENDED],
+    required: true,
+  })
+  @IsEnum([UserStatus.ACTIVE, UserStatus.SUSPENDED])
   status: UserStatus;
 }
